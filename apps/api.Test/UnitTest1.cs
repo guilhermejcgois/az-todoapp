964f91b85ec40cad5d7f13e21a6288cc737276d9
Guilherme Gois <guijocargo@gmail.com>
2024-04-23 23:18:16 -0300

    feat: creates c# API

---
+++ apps/api.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AzTodoapp.Api.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
